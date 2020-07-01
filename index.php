b7c2269be487aee91a439af0778fa4b89461c70a
Marknel Pogi <marknel.pineda23@gmail.com>
2020-07-01 15:25:18 +0800

    Testing: auto deployment 2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "test auto deployment";
+echo "test auto deployment 2";
 die();
 // header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");
 // header("Cache-Control: no-store, no-cache, must-revalidate");
